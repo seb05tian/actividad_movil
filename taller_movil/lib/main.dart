e5296093bd0218d91e5eba2b4805a7468fa3aabc
David Osorio <davidosoriosuarez17@gmail.com>
2024-04-13 00:00:31 -0500

    cambio para prueba

--- taller_movil/lib/main.dart
+++ taller_movil/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Dfffffffffffffffffffffffffffffffffffffffffffffffff'),
     );
   }
 }
@@ -77,3 +77,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
